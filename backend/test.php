73b34aa6692143c110f1e0cbdd380a50d831fe3e
Tiagotj7 <tiagowork07@gmail.com>
2025-12-03 16:27:40 -0300

    add test

---
+++ backend/test.php
@@ -0,0 +1,17 @@
+<?php
+header('Access-Control-Allow-Origin: https://user-manager-drab.vercel.app');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+header('Content-Type: application/json; charset=utf-8');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit;
+}
+
+echo json_encode([
+    'ok'     => true,
+    'method' => $_SERVER['REQUEST_METHOD'],
+    'origin' => $_SERVER['HTTP_ORIGIN'] ?? null,
+    'time'   => time(),
+]);

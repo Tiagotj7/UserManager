6529776224360a70510733a8aa74dad8a972f4c3
Tiagotj7 <tiagowork07@gmail.com>
2025-12-05 23:20:27 -0300

    test cors1

--- backend/index.php
+++ backend/index.php
@@ -20,7 +20,7 @@ if ($origin && in_array($origin, $allowedOrigins)) {
     header("Access-Control-Allow-Origin: *");
 }
 
-header('Access-Control-Allow-Origin: https://user-manager-red.vercel.app');
+header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header('Content-Type: application/json; charset=utf-8');
